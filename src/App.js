c5855d326ed1aa3997537bc4c2ab84bc892d314a
varun <varunswarup0@gmail.com>
2020-04-16 01:06:20 +0530

    Hot reloading with react-hot-loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
 
 const App = () => (
-    <div className="App">
-        <h1>Hello, World!</h1>
-    </div>
+  <div className="App">
+    <h1>Hello, World!!!</h1>
+  </div>
 );
 
-export default App;
+export default hot(module)(App);
