0d719072d0efa42d22dbb2277665b645f7b1d6b8
varun <varunswarup0@gmail.com>
2020-04-16 19:36:47 +0530

    Adding a THUNK to react

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import NewTodoForm from './NewTodoForm';
-import TodoListItem from './TodoListItem';
-import { removeTodo, markTodoAsCompleted } from './actions';
-import './TodoList.css';
+import React from "react";
+import { connect } from "react-redux";
+import NewTodoForm from "./NewTodoForm";
+import TodoListItem from "./TodoListItem";
+import { removeTodo, markTodoAsCompleted } from "./actions";
+import "./TodoList.css";
+import { displayAlert } from "./thunks";
 
 const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
-    <div className="list-wrapper">
-        <NewTodoForm />
-        {todos.map(todo => <TodoListItem
-            todo={todo}
-            onRemovePressed={onRemovePressed}
-            onCompletedPressed={onCompletedPressed}/>)}
-    </div>
+  <div className="list-wrapper">
+    <NewTodoForm />
+    {todos.map((todo) => (
+      <TodoListItem
+        todo={todo}
+        onRemovePressed={onRemovePressed}
+        onCompletedPressed={onCompletedPressed}
+      />
+    ))}
+  </div>
 );
 
-const mapStateToProps = state => ({
-    todos: state.todos,
+const mapStateToProps = (state) => ({
+  todos: state.todos,
 });
 
-const mapDispatchToProps = dispatch => ({
-    onRemovePressed: text => dispatch(removeTodo(text)),
-    onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
+const mapDispatchToProps = (dispatch) => ({
+  onRemovePressed: (text) => dispatch(removeTodo(text)),
+  onCompletedPressed: (text) => dispatch(markTodoAsCompleted(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
---
+++ src/todos/thunks.js
@@ -0,0 +1,3 @@
+export const displayAlert = (text) => () => {
+  alert(`Hello ${text}!`);
+};
