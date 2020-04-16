0607cf1ad4a69d33c7625df2f2526efc4211fd35
varun <varunswarup0@gmail.com>
2020-04-16 14:32:55 +0530

    Persisting the redux-store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 import { Provider } from "react-redux";
 import { configureStore } from "./store.js";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
+
+const store = configureStore();
+const persistor = persistStore(store);
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store.js
+++ src/store.js
@@ -1,8 +1,18 @@
 import { createStore, combineReducers } from "redux";
 import { todos } from "./todos/reducers";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
 
 const reducers = { todos };
 
 const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const configureStore = () => createStore(rootReducer);
+export const configureStore = () => createStore(persistedReducer);
