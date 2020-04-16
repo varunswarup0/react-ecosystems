bc98ee9d10332339d3049ba32aba45e97a89111a
varun <varunswarup0@gmail.com>
2020-04-16 13:40:10 +0530

    Running a react redux app

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './TodoListItem.css';
+import React from "react";
+import "./TodoListItem.css";
 
-const TodoListItem = ({ todo }) => (
-    <div className="todo-item-container">
-        <h3>{todo.text}</h3>
-        <div className="buttons-container">
-            <button className="completed-button">Mark As Completed</button>
-            <button className="remove-button">Remove</button>
-        </div>
+const TodoListItem = ({ todo, onRemovePressed }) => (
+  <div className="todo-item-container">
+    <h3>{todo.text}</h3>
+    <div className="buttons-container">
+      <button className="completed-button">Mark As Completed</button>
+      <button
+        onClick={() => onRemovePressed(todo.text)}
+        className="remove-button"
+      >
+        Remove
+      </button>
     </div>
+  </div>
 );
 
 export default TodoListItem;
